1aedd34943c37c5e9eaa0c1903e32db6f90109ec
albasilvent <albasilvent@gmail.com>
2024-08-07 15:54:28 +0200

    fixed_styles

--- climate-cast/src/app/[locale]/components/weatherCard/WeeklyForecast.tsx
+++ climate-cast/src/app/[locale]/components/weatherCard/WeeklyForecast.tsx
@@ -12,13 +12,16 @@ export default function WeeklyForecast({ weeklyForecast }: any) {
     const t = useTranslations("Page");
 
     return (
-        <section className="bg-white flex flex-col w-1/3 justify-between h-full rounded-lg shadow-gray-500 shadow-lg p-4">
+        <section className="bg-white flex flex-col w-1/3 justify-around h-full rounded-lg shadow-gray-500 shadow-lg p-4">
             <p className="text-lg w-full text-start">{t("5-day-forecast")}</p>
             {weeklyForecast &&
                 weeklyForecast.map((day: any, index: number) => {
                     return (
-                        <div key={index}>
-                            <div className="flex justify-around items-center">
+                        <div
+                            key={index}
+                            className="flex items-center justify-center w-full h-full px-2"
+                        >
+                            <div className="w-full flex justify-between items-center">
                                 <p className={`${lato_light.className}`}>
                                     {formatDate(day.dt_txt)}
                                 </p>
