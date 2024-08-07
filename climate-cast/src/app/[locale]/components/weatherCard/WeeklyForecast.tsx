408c45d06eea8170ff40e0824067817ffad23f4f
albasilvent <albasilvent@gmail.com>
2024-08-07 15:40:44 +0200

    space-between

--- climate-cast/src/app/[locale]/components/weatherCard/WeeklyForecast.tsx
+++ climate-cast/src/app/[locale]/components/weatherCard/WeeklyForecast.tsx
@@ -12,7 +12,7 @@ export default function WeeklyForecast({ weeklyForecast }: any) {
     const t = useTranslations("Page");
 
     return (
-        <section className="bg-white flex flex-col w-1/3 justify-around h-full rounded-lg shadow-gray-500 shadow-lg p-4">
+        <section className="bg-white flex flex-col w-1/3 justify-between h-full rounded-lg shadow-gray-500 shadow-lg p-4">
             <p className="text-lg w-full text-start">{t("5-day-forecast")}</p>
             {weeklyForecast &&
                 weeklyForecast.map((day: any, index: number) => {
